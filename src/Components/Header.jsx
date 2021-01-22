b3a5b58bd8cf47f647a93922d8c22c9bf1046e46
Nathaniel Rankin <rankinna@bc.edu>
2021-01-22 16:05:32 -0600

    Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../styles.css";
-import HeaderImage from "../img/banner.png";
 import Logo from "../img/logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav } from "react-bootstrap";
@@ -10,18 +9,17 @@ class Header extends React.Component {
     return (
       <div>
         <div className="header">
-          <h1>Facility Engineer Toolkit</h1>
-          <img src={HeaderImage} className="banner" alt="" />
-          <a href="http://bvartcc.com" target="_blank" rel="noreferrer">
-            <img src={Logo} className="logo" alt="BVA" />
+          <a
+            href="http://bvartcc.com"
+            target="_blank"
+            rel="noreferrer"
+            className="logo"
+          >
+            <img src={Logo} alt="BVA" />
           </a>
+          <h1>Facility Engineer Toolkit</h1>
         </div>
-        <Navbar
-          bg="dark"
-          variant="dark"
-          className="justify-content-center"
-          activeKey="/home"
-        >
+        <Navbar bg="dark" variant="dark" className="justify-content-center">
           <Nav>
             <Nav.Link active>Create a Sector File</Nav.Link>
             <Nav.Link>Convert to KML</Nav.Link>
